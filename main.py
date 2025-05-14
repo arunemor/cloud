79aa74d106e78bdb30c6b9b74b1912a9791638ba
arunemor <cloudemor2025@gmail.com>
2025-05-14 07:37:34 +0000

    WIP: local changes before revert

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ print("emor")
 print("phis")
 print("route53")
 print("vpc")
+
+
+
+arunemor ➜ /workspaces/cloud (main) $ git log --oneline
+888bf43 (HEAD -> main, origin/main, origin/HEAD)  add vpc
+1b14627 add route 53
+41912cf enter two new code
+d855386 new code
+1204513 Initial commit
