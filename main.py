10d9e82d291e749c980087441e130b9a66626cbe
arunemor <cloudemor2025@gmail.com>
2025-05-14 09:37:14 +0000

    new branch commit

--- main.py
+++ main.py
@@ -9,9 +9,3 @@ print("ec2")
 
 
 
-arunemor ➜ /workspaces/cloud (main) $ git log --oneline
-888bf43 (HEAD -> main, origin/main, origin/HEAD)  add vpc
-1b14627 add route 53
-41912cf enter two new code
-d855386 new code
-1204513 Initial commit
